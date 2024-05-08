57ae0dc5172f2032b7ba410d33961ce4f1500718
DaniM0613 <wwh.rj.92@gmail.com>
2024-05-08 12:09:52 -0600

    tipando la API con Zod, Volidot y Guards

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,13 @@ export type Country = {
     code: string
     name: string
 }
+
+export type Weather = {
+    name: string
+    main: {
+        temp: number
+        temp_max: number
+        temp_min: number
+        
+    }
+}
